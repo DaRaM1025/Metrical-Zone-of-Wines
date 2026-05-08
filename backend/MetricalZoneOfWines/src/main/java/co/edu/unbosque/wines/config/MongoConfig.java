73df771993d13a1944f4816b87282b0f4abfe45c
Santiago Pinzón Vásquez <Santipinzonv@gmail.com>
2026-05-07 23:55:58 -0500

    feat: documentación SQL y cambio de puerto mongo
    - Actualización de puerto de conexión a MongoDB Cluster (37017).
    - Inclusión de script Flyway V4 con funciones core documentadas para la sustentación.

--- backend/MetricalZoneOfWines/src/main/java/co/edu/unbosque/wines/config/MongoConfig.java
+++ backend/MetricalZoneOfWines/src/main/java/co/edu/unbosque/wines/config/MongoConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
-public class MongoConfig extends AbstractMongoClientConfiguration {
+public class    MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     protected String getDatabaseName() {
@@ -20,7 +20,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
         // Aquí pegamos la URI directamente.
         // El # se pone como %23 para que el driver no se confunda.
         ConnectionString connectionString = new ConnectionString(
-                "mongodb://wines_user:Password123%23@54.156.124.164:27017/wines_db?authSource=admin"
+                "mongodb://wines_user:Password123%23@54.156.124.164:37017/wines_db?authSource=admin"
         );
 
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
